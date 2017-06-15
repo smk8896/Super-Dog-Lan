62356a43877f7e33fd64cce0f91e242adad76dd0
Shea Matthew Kennisher <deidrelarroque@Deidres-MacBook-Air.local>
2017-06-15 10:30:36 -0500

    Add Breeds table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615152159) do
+ActiveRecord::Schema.define(version: 20170615152840) do
+
+  create_table "breeds", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "owners", force: :cascade do |t|
     t.string "first_name"
